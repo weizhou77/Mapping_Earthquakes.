d4581faec67cd11e0172ee2a3b074746de495efe
weizhou77 <92563285+weizhou77@users.noreply.github.com>
2022-01-19 16:24:44 -0800

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 // Create the map object with a center and zoom level
 // from the Leaflet Quick Start Guide page, change the gergraphical center of the map to the US
 
-let map = L.map('mapid').setView([37.6213, -122.3790], 10);
+//let map = L.map('mapid').setView([40.7,-94.5], 4); //([centerLatitude, centerLontitude], zoomLevel)
 
 // above: 'mapid' will reference the id tag in our <div> element on the index.html file
 // setView() method sets the view of the map with a geographical center
@@ -25,10 +25,10 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 10);
 // copy from the Leaflet Quick Start Guide
 // We create the tile layer that will be the background of our map.
 // the first link is the mapbox styles, we can check the mapbox Docs - mapbox styles website to check
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {    
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {    
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    //id: 'mapbox/streets-v11',
+    id: 'streets-v11',
     //tileSize: 512,
     //zoomOffset: -1,
     accessToken: API_KEY
@@ -45,11 +45,21 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 //mapbox/satellite-v9
 //mapbox/satellite-streets-v11
 
-
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// ********* for 'Add Multiple Maps' section, we dont add the streets.addTo(map);
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! for other sections, we need to uncomment it.
+
+// streets.addTo(map);
+
 // above: we call the addTo() function with our map object, map on our graymap object tile layer
 // the addTo() function will add the graymap object tile layer to our let map
+
+//******************************** */
+// For Mapping Multiple GeoJSON Points
+//****************************** */
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/weizhou77/Mapping_Earthquakes./main/majorAirports.json"
+
 
 // ******************************** 
 //Add a Marker to the Map
@@ -140,27 +150,27 @@ let cityData = cities;
  //Map a GeoJSON point
 
  // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}} // reverse order, becaue GeoJSON data coordinates are set with the first parameter as X(longitude) and second parameter as Y(latitude).
-]};
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}} // reverse order, becaue GeoJSON data coordinates are set with the first parameter as X(longitude) and second parameter as Y(latitude).
+// ]};
 
 // create the GeoJSON layer and add it to out map
-L.geoJSON(sanFranAirport).addTo(map);
+// L.geoJSON(sanFranAirport).addTo(map);
 
 //Build a popup tp the marker
 
@@ -175,9 +185,55 @@ L.geoJSON(sanFranAirport).addTo(map);
 // }).addTo(map);
 
 // Grabbing our GeoJSON data
-L.geoJSON(sanFranAirport, {
-    onEachFeature : function(feature, layer) {
-        console.log(layer);
-        layer.binPopup("<h3>" + "Airport code:" + feature.properties.faa + "</h3> <hr><h3>" + "Airport name: " + feature.properties.name + "</h3>");
-    }
-}).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature : function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup("<h3>" + "Airport code:" + feature.properties.faa + "</h3> <hr><h3>" + "Airport name: " + feature.properties.name + "</h3>");
+//     }
+// }).addTo(map);
+
+//************************************** */
+// Mapping Multiple GeoJSON Points
+//*************************************** */
+
+//******************************** */
+// For Mapping Multiple GeoJSON Points
+//****************************** */
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/weizhou77/Mapping_Earthquakes./main/majorAirports.json"
+
+
+//Grabbing our GeoJSON data
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJSON(data).addTo(map);
+});
+
+// **********************************
+// Add Multiple Maps, or multile base layers and overlays
+// **********************************
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'dark-v10',
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps
+let baseMaps = {
+  Street: streets,
+  Dark : dark
+};
+
+// create the map object with center, zoom level and default layer
+let map = L.map('mapid', {
+  center : [30,30],
+  zoom:2,
+  layers:[streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
