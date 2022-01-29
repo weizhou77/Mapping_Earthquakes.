4277b6bb19c64f82f0dbb1a26bd3240400d6ed35
weizhou77 <92563285+weizhou77@users.noreply.github.com>
2022-01-28 17:52:11 -0800

    Add files via upload

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -123,16 +123,16 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   allEarthquakes.addTo(map);
 
 // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
 // 4. Use the same style as the earthquake data.
-function styleInfo(feature) {
+function styleInfoMajorE(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: getColorMajorE(feature.properties.mag),
     color: "#000000",
-    radius: getRadius(feature.properties.mag),
+    radius: getRadiusMajorE(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
@@ -140,21 +140,21 @@ function styleInfo(feature) {
 
 
 // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-function getColor(magnitude) {
+function getColorMajorE(magnitude) {
   if (magnitude > 5 && magnitude < 6) {
     return "#ea2c2c";
   }
-  if (magnitude < 5) {
+  if (magnitude <= 5) {
     return "#ea822c";
   }
-  if (magnitude > 6) {
-  return "#98ee00";
+  if (magnitude >= 6) {
+  return "#770737";
 }
 };
 
 
 // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
-function getRadius(magnitude) {
+function getRadiusMajorE(magnitude) {
   if (magnitude === 0) {
     return 1;
   }
@@ -172,7 +172,7 @@ L.geoJson(data, {
       return L.circleMarker(latlng);
     },
   // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
+style: styleInfoMajorE,
  // We create a popup for each circleMarker to display the magnitude and location of the earthquake
  //  after the marker has been created and styled.
  onEachFeature: function(feature, layer) {
@@ -194,14 +194,15 @@ let legend = L.control({
 legend.onAdd = function() {
   let div = L.DomUtil.create("div", "info legend");
 
-  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const magnitudes = [0, 1, 2, 3, 4, 5,6];
   const colors = [
     "#98ee00",
     "#d4ee00",
     "#eecc00",
     "#ee9c00",
     "#ea822c",
-    "#ea2c2c"
+    "#ea2c2c",
+    "#770737"
   ];
 
 // Looping through our intervals to generate a label with a colored square for each interval.
